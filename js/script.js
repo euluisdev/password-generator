78a37e35517c5466f18f5edde8cc839800216701
euluis-dev <fluisf00@gmail.com>
2024-03-24 13:12:22 -0300

    reated function generatePasswBtn and add event click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-'strict'
+'use strict'
 
+const generatePasswBtn = document.querySelector('#generete-password');
+const generatorPasswElement = document.querySelector('#generated-password');
+
+    if (!generatePasswBtn || !generatorPasswElement) {
+        throw Error ('Elementos DOM não encontrados!');
+    }
+
+generatePasswBtn.addEventListener('click', () => {
+    console.log ('Hellow Worldão!');
+});
 
